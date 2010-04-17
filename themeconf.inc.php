b1ccde6bdcca4ba7fb8e573271d19f83d18e57b2
flop25 <flop25@piwigo.org>
2010-04-17 18:53:52 +0000

    first commit ;  2° step
    git-svn-id: http://piwigo.org/svn/extensions/floPure/Pure_tr_clear_blue@5913 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ themeconf.inc.php
@@ -0,0 +1,9 @@
+<?php
+$themeconf = array(
+  'name'         => 'Pure_tr_clear_blue',
+  'parent'        => 'Pure_default',
+  'icon_dir'      => 'themes/Pure_default/icon',
+  'mime_icon_dir' => 'themes/Pure_default/icon/mimetypes/',
+);
+
+?>
